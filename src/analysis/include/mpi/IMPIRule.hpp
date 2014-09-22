bd0a2686053bced0f8d07eae7a9afd04c56f9e24
fschmitt <fschmitt@7cca49d6-365a-11e3-882a-ebbae9fb0804>
2014-09-22 14:56:17 +0000

    Add missing rule interface files

---
+++ src/analysis/include/mpi/IMPIRule.hpp
@@ -0,0 +1,55 @@
+/*
+ * This file is part of the CASITA software
+ *
+ * Copyright (c) 2014,
+ * Technische Universitaet Dresden, Germany
+ *
+ * This software may be modified and distributed under the terms of
+ * a BSD-style license. See the COPYING file in the package base
+ * directory for details.
+ *
+ */
+
+#pragma once
+
+#include <mpi.h>
+
+#include "AbstractRule.hpp"
+#include "AnalysisEngine.hpp"
+#include "MPIRulesCommon.hpp"
+
+namespace casita
+{
+ namespace mpi
+ {
+  class AnalysisParadigmMPI;
+
+  class IMPIRule :
+    public AbstractRule
+  {
+    public:
+      IMPIRule( const char* name, int priority ) :
+        AbstractRule( name, priority )
+      {
+
+      }
+
+      virtual
+      ~IMPIRule( )
+      {
+      }
+
+      bool
+      applyRule( AnalysisEngine* analysis, GraphNode* node )
+      {
+        return apply( (AnalysisParadigmMPI*)analysis->getAnalysisParadigm(
+                        PARADIGM_MPI ), node );
+      }
+
+    protected:
+      virtual bool
+      apply( AnalysisParadigmMPI* analysis, GraphNode* node ) = 0;
+
+  };
+ }
+}
