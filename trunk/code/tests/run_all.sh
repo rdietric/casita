89ddfb14f2353215325362dab5c78e93fc039db0
fschmitt <fschmitt@7cca49d6-365a-11e3-882a-ebbae9fb0804>
2014-10-10 14:47:51 +0000

    Basic runtime testing works using run_all.sh Added perl script to run on each trace Added traces for testing purposes

--- trunk/code/tests/run_all.sh
+++ trunk/code/tests/run_all.sh
@@ -13,20 +13,37 @@
 
 # set some variables
 EXE=casita
+PERL=perl
+TEST_SCRIPT=test_trace.pl
 TRACE_INPUT_DIR=traces
-NUM_TESTS=0
+NUM_TESTS_SUCCESS=0
 NUM_TOTAL_TESTS=0
+TRACE_OUTPUT_DIR=
 
 # functions
 
-function check_setup () {
+function check_setup {
     # find casita
     command -v $EXE &> /dev/null || { echo "Could not find CASITA executable, abort." >&2; return 1; }
+    command -v $PERL &> /dev/null || { echo "Could not find perl executable, abort." >&2; return 1; }
 
     # try to run casita
     $EXE --help | grep "casita" &> /dev/null
     if [ $? -ne 0 ]; then
         echo "CASITA does not seem to work, abort." >&2
+        return 1
+    fi
+
+    # try to run perl
+    $PERL --help | grep "perl" &> /dev/null
+    if [ $? -ne 0 ]; then
+        echo "perl does not seem to work, abort." >&2
+        return 1
+    fi
+
+    # find perl test script
+    if [ ! -f "$TEST_SCRIPT" ]; then
+        echo "Did not find perl test script '$TEST_SCRIPT', abort." >&2
         return 1
     fi
 
@@ -39,13 +56,28 @@ function check_setup () {
     return 0
 }
 
-function run_tests () {
+function run_single_test {
+    echo "Testing '$1'" >&2
+
+    $PERL $TEST_SCRIPT $1 $TRACE_OUTPUT_DIR
+}
+
+function run_tests {
+    for dir in $(ls -d traces/*/); do
+        ((NUM_TOTAL_TESTS += 1))
+        run_single_test $dir
+        if [ $? -eq 0 ]; then
+            ((NUM_TESTS_SUCCESS += 1))
+        fi
+    done
+
     return 0
 }
 
 
 # main
-if [[ $(check_setup) -ne 0 ]]; then
+check_setup
+if [ $? -ne 0 ]; then
     exit 1
 fi
 
@@ -55,9 +87,10 @@ echo "Created temporary dir '$TRACE_OUTPUT_DIR' for tests" >&2
 
 # run tests
 
-res=$(run_tests)
-echo "Tests: $NUM_TESTS/$NUM_TOTAL_TESTS" >&2
-if [[ $res -ne 0 ]]; then
+run_tests
+tests_result=$?
+echo "Tests: $NUM_TESTS_SUCCESS/$NUM_TOTAL_TESTS" >&2
+if [ $tests_result -ne 0 ]; then
     echo "FAILED" >&2
 else
     echo "SUCCESS" >&2
